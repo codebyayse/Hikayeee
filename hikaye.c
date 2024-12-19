3448e1c1d48f66d848a87d901b12d428b2eb7695
Ayşe Nur KARAKOYUNLU <ayse.nur7krkynl@gmail.com>
2024-12-19 16:03:59 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -5,6 +5,7 @@ int main() {
 
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
-printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
+printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..");
+printf("Önlerine çıkan çılgın palyaçonun çok zor bir engel olmadığını düşünerek balonlarının bir kısmını çalıp bir kısmını gökyüzüne bırakarak yollarına devam etmeye karar verdiler.);//Ayşe Nur Karakoyunlu
 
 }
